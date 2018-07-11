6bb9381a67375bf36c22629b34be2f76f2884165
Wojtek Bojarczuk <wojtekboj@gmail.com>
2018-07-11 14:22:41 +0200

    Modification scripts.js

--- script.js
+++ script.js
@@ -21,83 +21,46 @@ class App extends React.Component {
         const url = `https://api.github.com/search/users?q=${searchText}`;
         fetch(url)
             .then(response => response.json())
-            .then(responseJson => this.setState({
-                users: responseJson.items
-            }));
+            .then(responseJson => this.setState({users: responseJson.items}));
     }
 
     render() {
-        return ( < div >
-            <
-            form onSubmit = {
-                event => this.onSubmit(event)
-            } >
-            <
-            label htmlFor = "searchText" > Search by user name < /label> <
-            input type = "text"
-            id = "searchText"
-            onChange = {
-                event => this.onChangeHandle(event)
-            }
-            value = {
-                this.state.searchText
-            }
-            /> < /
-            form > <
-            UsersList users = {
-                this.state.users
-            }
-            /> < /
-            div >
+        return ( 
+            <div>
+                <form onSubmit = {event => this.onSubmit(event)} >
+                    <label htmlFor = "searchText" > Search by user name < /label> 
+                    <input type = "text"
+                    id = "searchText"
+                    onChange = {event => this.onChangeHandle(event)}
+                    value = {this.state.searchText}/>
+                </form>
+                <UsersList users = {this.state.users}/>
+            </div>
         );
     }
 }
 
 class UsersList extends React.Component {
         get users() {
-            return this.props.users.map(user => < User key = {
-                    user.id
-                }
-                user = {
-                    user
-                }
-                />);
+            return this.props.users.map(user => <User key={user.id} user={user}/>);
             }
 
             render() {
-                return ( <
-                    div className = "listOfUsers" > {
-                        this.users
-                    } <
-                    /div>
+                return ( 
+                    <div className="listOfUsers"> 
+                        {this.users} 
+                    </div>
                 );
             }
         }
 
         class User extends React.Component {
             render() {
-                return ( <
-                    div className = {
-                        "users"
-                    } >
-                    <
-                    img src = {
-                        this.props.user.avatar_url
-                    }
-                    style = {
-                        {
-                            maxWidth: '100px'
-                        }
-                    }
-                    /><br/ >
-                    <
-                    a href = {
-                        this.props.user.html_url
-                    }
-                    target = "_blank" > {
-                        this.props.user.login
-                    } < /a> < /
-                    div >
+                return ( 
+                    <div className={"users"}>
+                        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/><br/>
+                        <a href={this.props.user.html_url} target="_blank" > {this.props.user.login} < /a> 
+                    </div >
                 );
             }
         }
